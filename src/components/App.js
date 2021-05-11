681934b5b71a40afc5f76b0e0391a9da29642bf3
Mohamed Salah <salahineo.personal@gmail.com>
2021-05-11 20:16:03 +0200

    Restore HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 // Libraries
 import React from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
 
 // Pages
 import ReadNotes from "../pages/ReadNotes";
@@ -22,7 +22,7 @@ import AppContextProvider from "../contexts/AppContext";
 // Component
 const App = () => {
   return (
-    <Router basename='/smart-notes'>
+    <Router>
       <AppContextProvider>
         <Layout>
           <Switch>
