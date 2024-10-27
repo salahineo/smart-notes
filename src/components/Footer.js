3ecbc67d9b267c3a9b1c1761a4a389d3971619bb
Mohamed Salah <salahineo.personal@gmail.com>
2024-10-27 19:33:09 +0300

    fix: refactor the app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ const Footer = () => {
         fontFamily: "\"Roboto\", \"Helvetica\", \"Arial\", sans-serif",
         display: "flex",
         alignItems: "center",
-        justifyContent: "space-between",
+        justifyContent: "center",
         backgroundColor: state.profile.theme === "light" ? "#ffffff" : "#333333",
         width: "calc(100% - 30px)",
         padding: 15,
@@ -35,9 +35,10 @@ const Footer = () => {
       },
       copyright: {
         color: state.profile.theme === "light" ? "#222222" : "#ffffff",
-        "& > span:last-of-type": {
+        "& > a": {
           color: state.profile.theme === "light" ? "#3f51b5" : "#7f91ff",
-          fontWeight: "bold"
+          fontWeight: "bold",
+          textDecoration: "none"
         }
       },
       link: {
@@ -61,27 +62,7 @@ const Footer = () => {
   return (
     <footer className={classes.footer}>
       <div className={classes.copyright}>
-        © <span>{String(new Date().getFullYear())}</span> | <span className="author">Mohamed Salah</span>
-      </div>
-      <div className="links">
-        <a className={classes.link} href="https://github.com/salahineo" target="_blank" rel="noreferrer">
-          <i className="fab fa-github" />
-        </a>
-        <a className={classes.link} href="https://www.linkedin.com/in/salahineo/" target="_blank" rel="noreferrer">
-          <i className="fab fa-linkedin-in" />
-        </a>
-        <a className={classes.link} href="https://twitter.com/salahineo" target="_blank" rel="noreferrer">
-          <i className="fab fa-twitter" />
-        </a>
-        <a className={classes.link} href="https://www.facebook.com/salahineo/" target="_blank" rel="noreferrer">
-          <i className="fab fa-facebook-f" />
-        </a>
-        <a className={classes.link} href="https://salahineo.github.io/salahineo/" target="_blank" rel="noreferrer">
-          <i className="fas fa-globe-africa" />
-        </a>
-        <a className={classes.link} href="mailto:salahineo.work@gmail.com" target="_blank" rel="noreferrer">
-          <i className="fas fa-envelope" />
-        </a>
+        © <span>{String(new Date().getFullYear())}</span> | <a href='https://linktree.salahineo.com' rel="noopener" target="_blank" className="author">Mohamed Salah</a>
       </div>
     </footer>
   );
